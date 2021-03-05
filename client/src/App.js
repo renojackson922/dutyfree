faf854251a624383f9dba5d096f3a0ef7ecf3878
Margarette <renojackson922@gmail.com>
2021-03-05 17:38:41 +0900

    일부 디자인 수정

--- client/src/App.js
+++ client/src/App.js
@@ -7,27 +7,28 @@ function App() {
   return (
     <>
       <Header />
+      <Mp3Test/>
       <div className="container text-center">
         <div className="mt-2" style={{maxWidth:"500px", margin:"0 auto"}}>
           <div className="mt-2 mb-2 row">
             <div className="col-4 text-start" style={{fontSize:"2.5rem"}}>
-            <a className="btn btn-secondary"><i className="fas fa-chevron-left"></i></a>
+            <a className="btn btn-secondary" href="#" onClick=""><i className="fas fa-chevron-left"></i></a>
             </div>
             <div className="col-4 text-center" style={{display: "flex", justifyContent: "center", alignItems: "center" }}>
               <span style={{fontSize:"1.5rem"}}>2021.04</span>
             </div>
             <div className="col-4 text-end" style={{fontSize: '2.5rem'}}>
-              <a className="btn btn-secondary"><i className="fas fa-chevron-right"></i></a>
+              <a className="btn btn-secondary" href="#" onClick=""><i className="fas fa-chevron-right"></i></a>
             </div>
           </div>
           <Calendar2 year={2021} month={3} />
           <div className="card">
             <div className="card-header">듀티통계</div>
             <div className="card-body">
-              <google-chart data='[["Month", "Days"], ["Jan", 31]]'></google-chart>
-              {/* <h5 class="card-title">Special title treatment</h5>
+              {/* <google-chart data='[["Month", "Days"], ["Jan", 31]]'></google-chart> */}
+              <h5 class="card-title">Special title treatment</h5>
               <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a> */}
+              <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
